3937b96df0af47cccd6fa7e62059c2f296ad652b
Marat <milya010790@mail.ru>
2024-08-07 13:55:41 +0500

    Создали контекст БД

---
+++ SocialNetwork.Web/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Web.Models.Users;
+
+namespace SocialNetwork.Web.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
