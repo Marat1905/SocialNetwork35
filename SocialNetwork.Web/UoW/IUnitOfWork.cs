584cc64ea50a12e3403f4d21e65ff7f4071728ba
Marat <milya010790@mail.ru>
2024-08-07 15:43:11 +0500

    Создали хранилище всех репозиториев

---
+++ SocialNetwork.Web/UoW/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using SocialNetwork.Web.Repository;
+
+namespace SocialNetwork.Web.UoW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int SaveChanges(bool ensureAutoHistory = false);
+
+        IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = true) where TEntity : class;
+
+    }
+}
