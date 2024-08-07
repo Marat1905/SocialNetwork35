ebd1b67c38e76569c91992373425c61f96193954
Marat <milya010790@mail.ru>
2024-08-07 13:55:19 +0500

    Создание модели пользователя

---
+++ SocialNetwork.Web/Models/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SocialNetwork.Web.Models.Users
+{
+    public class User: IdentityUser
+    {
+        /// <summary>Имя</summary>
+        public string FirstName { get; set; }
+
+        /// <summary>Фамилия</summary>
+        public string LastName { get; set; }
+
+        /// <summary>Отчество</summary>
+        public string MiddleName { get; set; }
+
+        /// <summary>Дата рождения</summary>
+        public DateTime BirthDate { get; set; }
+    }
+}
