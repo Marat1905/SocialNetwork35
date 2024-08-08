027a24e06e8ffcbb06a9f63690890ec0ca5111c3
Marat <milya010790@mail.ru>
2024-08-08 11:12:00 +0500

    Конфигурация для друзей в БД

---
+++ SocialNetwork.Web/Configs/FriendConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Web.Models.Users;
+
+namespace SocialNetwork.Web.Configs
+{
+    public class FriendConfiguration : IEntityTypeConfiguration<Friend>
+    {
+
+        public void Configure(EntityTypeBuilder<Friend> builder)
+        {
+            builder.ToTable("UserFriends").HasKey(p => p.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+        }
+    }
+}
