c9d506d344516bb16d32754fdcad69f175cbc285
Marat <milya010790@mail.ru>
2024-08-08 20:31:27 +0500

    рефакторинг

--- SocialNetwork.Web/Controllers/Account/AccountManagerController.cs
+++ SocialNetwork.Web/Controllers/Account/AccountManagerController.cs
@@ -222,7 +222,12 @@ namespace SocialNetwork.Web.Controllers.Account
 
             var result = await _userManager.GetUserAsync(currentuser);
 
-            var list = _userManager.Users.AsEnumerable().Where(x => x.GetFullName().ToLower().Contains(search.ToLower())).ToList();
+            var list = _userManager.Users.AsEnumerable().ToList();
+            if (!string.IsNullOrEmpty(search))
+            {
+                list = list.Where(x => x.GetFullName().ToLower().Contains(search.ToLower())).ToList();
+            }
+
             var withfriend = await GetAllFriend();
 
             var data = new List<UserWithFriendExt>();
