927a7470f8749a7bd5bbad451011f2e9670ca620
Marat <milya010790@mail.ru>
2024-08-08 10:07:28 +0500

    Расширение для модели

---
+++ SocialNetwork.Web/Extentions/UserFromModel.cs
@@ -0,0 +1,23 @@
+﻿using SocialNetwork.Web.Models.Users;
+using SocialNetwork.Web.ViewModels.Account;
+
+namespace SocialNetwork.Web.Extentions
+{
+    public static class UserFromModel
+    {
+        public static User Convert(this User user, UserEditViewModel usereditvm)
+        {
+            user.Image = usereditvm.Image;
+            user.LastName = usereditvm.LastName;
+            user.MiddleName = usereditvm.MiddleName;
+            user.FirstName = usereditvm.FirstName;
+            user.Email = usereditvm.Email;
+            user.BirthDate = usereditvm.BirthDate;
+            user.UserName = usereditvm.UserName;
+            user.Status = usereditvm.Status;
+            user.About = usereditvm.About;
+
+            return user;
+        }
+    }
+}
