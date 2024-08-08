13f8b1b0c6bf42209b6dcd62bcb78fd7eb06bd83
Marat <milya010790@mail.ru>
2024-08-08 09:52:46 +0500

    Изменение модели пользователя

--- SocialNetwork.Web/Models/Users/User.cs
+++ SocialNetwork.Web/Models/Users/User.cs
@@ -11,9 +11,28 @@ namespace SocialNetwork.Web.Models.Users
         public string LastName { get; set; }
 
         /// <summary>Отчество</summary>
-        public string MiddleName { get; set; }
+        public string? MiddleName { get; set; }
 
         /// <summary>Дата рождения</summary>
         public DateTime BirthDate { get; set; }
+
+        public string Image { get; set; }
+
+        public string Status { get; set; }
+
+
+        public string About { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + MiddleName + " " + LastName;
+        }
+
+        public User()
+        {
+            Image = "https://via.placeholder.com/500";
+            Status = "Ура! Я в соцсети!";
+            About = "Информация обо мне.";
+        }
     }
 }
