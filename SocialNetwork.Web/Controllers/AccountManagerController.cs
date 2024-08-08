a30516461ffcdd22c085706dc873c7e7daa914b3
Marat <milya010790@mail.ru>
2024-08-08 12:55:14 +0500

    Создание метода добавления в друзья

--- SocialNetwork.Web/Controllers/AccountManagerController.cs
+++ SocialNetwork.Web/Controllers/AccountManagerController.cs
@@ -106,7 +106,23 @@ namespace SocialNetwork.Web.Controllers
             return View("UserList", model);
         }
 
+        [Route("AddFriend")]
+        [HttpPost]
+        public async Task<IActionResult> AddFriend(string id)
+        {
+            var currentuser = User;
 
+            var result = await _userManager.GetUserAsync(currentuser);
+
+            var friend = await _userManager.FindByIdAsync(id);
+
+            var repository = _unitOfWork.GetRepository<Friend>() as FriendsRepository;
+
+            repository.AddFriend(result, friend);
+
+
+            return RedirectToAction("MyPage", "AccountManager");
+        }
 
         [Authorize]
         [Route("Update")]
