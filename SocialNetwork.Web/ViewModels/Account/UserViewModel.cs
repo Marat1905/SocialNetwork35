6cace17ddd84e5d83be162adb0360b46de3b8994
Marat <milya010790@mail.ru>
2024-08-08 07:34:03 +0500

    Добавлена модель для пользователя

---
+++ SocialNetwork.Web/ViewModels/Account/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SocialNetwork.Web.Models.Users;
+
+namespace SocialNetwork.Web.ViewModels.Account
+{
+    public class UserViewModel
+    {
+        public User User { get; set; }
+
+        public UserViewModel(User user)
+        {
+            User = user;
+        }
+
+        public List<User> Friends { get; set; }
+
+    }
+}
